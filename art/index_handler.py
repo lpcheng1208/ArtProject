4de6c3fc0fdee7159cdb4eab88ebdc301807d2ef
lpc1208 <lpc00972@gmail.com>
2018-05-29 19:37:51 +0800

    django项目

--- art/index_handler.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.shortcuts import render, HttpResponse
-
-__author__ = 'lpc'
-__date__ = '2018/5/29 15:03'
-
-
-def index(request):
-    return render(request, 'index.html')
